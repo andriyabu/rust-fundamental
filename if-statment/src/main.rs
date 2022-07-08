ff98783e24b08d6caf5360d6a797d9bf0d8f7f80
Andri Yabu Ndapa Rehing <andriyabu@gmail.com>
2022-07-08 10:39:01 +0700

    if else statement

---
+++ if-statment/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x = 50;
+
+    if x == 45 {
+        println!("x is equal to {}", x);
+    }else if x > 45 {
+        println!("x is greater than 45");
+    }else{
+        println!("x is less than 45");
+    }
+    //println!("Hello, world!");
+}
