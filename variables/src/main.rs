d0b7d850264b9983d9c0e26dfe14001c6efad8c4
Andri Yabu Ndapa Rehing <andriyabu@gmail.com>
2022-07-08 09:55:34 +0700

    specified variables data types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -5,4 +5,13 @@ fn main() {
 
     x = 65;
     println!("The value of x is {}", x);
+
+    // specified variables data types
+    let a:i64 = 32; // i64
+    let f:f32 = 5.7; // f32
+    let b:bool = false; // bool
+
+    println!("The value of a is {} integer 64", a);
+    println!("The value of f is {} float 32", f);
+    println!("The value of b is {} bool", b);
 }
